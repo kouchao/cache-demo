ee44b3874604483b4742667c81151add2ee4088f
kouchao <1396726559@qq.com>
2019-12-04 13:57:00 +0000

    fix: 创建中间件仅运行一次定时任务

--- src/cache.js
+++ src/cache.js
@@ -2,47 +2,51 @@ const CronJob = require('cron').CronJob
 
 let cacheStore = {}
 
-module.exports = (conf = {}) => async (ctx, next) => {
-
+module.exports = (conf = {}) => {
+ 
   let { urls, cronTime = '0 0 0 * * * *' } = conf
+ 
   // 定时任务
   new CronJob(cronTime, function() {
     cacheStore = {}
   }, null, true, 'America/Los_Angeles')
+  
+  return async (ctx, next) => {
 
-  // 类型检查
-  if (urls && typeof urls !== 'string' && !Array.isArray(urls)) {
-    console.error('urls必须是一个数组或字符串')
-    urls = ''
-  }
+    // 类型检查
+    if (urls && typeof urls !== 'string' && !Array.isArray(urls)) {
+      console.error('urls必须是一个数组或字符串')
+      urls = ''
+    }
 
-  // 如果urls存在 并且 不是数组
-  if (urls && !Array.isArray(urls)) {
-    urls = [urls]
-  }
+    // 如果urls存在 并且 不是数组
+    if (urls && !Array.isArray(urls)) {
+      urls = [urls]
+    }
 
 
-  const isMatching = urls ? urls.some(url => url === ctx.url) : true
+    const isMatching = urls ? urls.some(url => url === ctx.url) : true
 
-  // 是否匹配路由
-  if (!isMatching) {
-    await next()
-    return
-  }
+    // 是否匹配路由
+    if (!isMatching) {
+      await next()
+      return
+    }
 
-  const cache = cacheStore[ctx.url]
+    const cache = cacheStore[ctx.url]
 
   
-  if (cache) {
+    if (cache) {
 
-    // 缓存命中
-    ctx.body = cache
-  } else {
-    await next()
+      // 缓存命中
+      ctx.body = cache
+    } else {
+      await next()
 
-    const data = ctx.body
+      const data = ctx.body
 
-    // 缓存数据
-    cacheStore[ctx.url] = data
+      // 缓存数据
+      cacheStore[ctx.url] = data
+    }
   }
 }
